fd7c1c0c670728466f34c145808d3cee587c2f91
Rajat Thakur <rajatthakur0998870@gmail.com>
2024-07-18 13:41:36 +0530

    lesson 3

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Namaste React 
+
+
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File watching algorithm - written c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification 
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code Spliting
+- Differential Bundling - Support Older browsers
+- Diagnostic
+- Error Handling
+- Https
+- Tree Shaking - remove unused code
+- Different dev and production bundle
--- app.js
+++ app.js
@@ -1,5 +1,51 @@
-const heading = React.createElement("h1", {}, "Hello World From React Inside");
+import React from "react";
+import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const parentDiv = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", { id: "heading" }, "Hello world from React with nested div"), React.createElement("h2", { id: "heading" }, "Hello world from React with nested div second")]));
-root.render(parentDiv);
+// const heading = React.createElement("h1", {id:"heading"}, "hello react");
+
+// React.createElement creates js object for html rendering in with react DOM
+
+// JSX == HTML-like or XML-like syntax
+
+// JSX babel transpiles it to create React.createElement creates js object for html rendering in with react DOM
+
+// React Element
+const jsxHeading = <h1>Hello React using jsx</h1>;
+
+// React functional component 
+
+const HeadingFunction = () => <h1>Hello React using jsx from functional component</h1>
+
+// component composition is to use two components into one 
+const Heading = () => <h1>Hello React using jsx from functional component</h1>
+const SubHeading = () => <h2>Hello React using jsx from functional component</h2>
+const Container = () => (
+    <div>
+        <p>Boath Headings comes witn component composition</p>
+        {console.log('hello console')}
+        {jsxHeading}
+        <Heading />
+        <SubHeading />
+    </div>
+)
+root.render(<Container />);
+
 // root.render(heading);
+
+
+// Swiggy food ordering app
+/**
+ * Header
+ *  - Logo
+ *  - Nav Item
+ * Body
+ *  - Search 
+ *  - Restaurant Container 
+ *      - Restaurant Card
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact 
+ * 
+ */
